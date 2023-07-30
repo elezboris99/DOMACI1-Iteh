92f2efee1a6ae5909dfe7a4f0a554b56992db13d
Boris Elez <elez.boris@gmail.com>
2023-07-31 00:28:30 +0200

    odradjene 2 crud operacije i zapoceta izmena

--- home.php
+++ home.php
@@ -25,10 +25,12 @@ if ($result->num_rows == 0) {
 <head>
     <title>Lista narudžbi</title>
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
 <body>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <div id="header">
         <h1>Lista narudžbi</h1>
     </div>
@@ -40,11 +42,11 @@ if ($result->num_rows == 0) {
                 <li>
                     <strong><?php echo $red["proizvod"] ?></strong>
                     <div class="order-info"><?php echo $red["id"] ?></div>
-                    <div class="order-info"><?php echo $red["kolicina"] ?></div>
-                    <div class="order-info"><?php echo $red["mjera"] ?></div>
-                    <div class="added-by"><?php echo $red["dodao"] ?></div>
+                    <div class="order-info">Kolicina: <?php echo $red["kolicina"] ?></div>
+                    <div class="order-info">Mjera: <?php echo $red["mjera"] ?></div>
+                    <div class="added-by">Dodao: <?php echo $red["dodao"] ?></div>
                     <label class="radio-btn">
-                        <input type="radio" name="checked-donut" value="obiljezen">
+                        <input type="radio" name="checked-donut" value="<?php echo $red["id"] ?>">
                     </label>
                 </li>
             <?php
@@ -54,7 +56,7 @@ if ($result->num_rows == 0) {
     </div>
 
 
- 
+
     <div class="container">
         <div class="box">
             <h4>Sakrij/Prikazi spisak</h4>
@@ -62,7 +64,7 @@ if ($result->num_rows == 0) {
         </div>
         <div class="box">
             <h4>Izmeni</h4>
-            <img src="img/change.png" id="icon">
+          <button id="btnizmeniNarudzbu" onclick="provjeriOznaku()"><img src="img/change.png" id="icon"></button>  
         </div>
         <div class="box">
             <h4>Obrisi</h4>
@@ -70,34 +72,34 @@ if ($result->num_rows == 0) {
         </div>
         <div class="box">
             <h4>Dodaj novi</h4>
-           <button id="dodajNarudzbu"><img src="img/add.png" id="icon"></button> 
+            <button id="dodajNarudzbu"  ><img src="img/add.png" id="icon"></button>
         </div>
     </div>
 
     <div class="formaNoviProizvod" id="dodajModel" style="display: none;">
-        <form action="action_page.php">
+        <form action="#" name="NoviProizvod" method="post" id="dodajForm">
             <div class="row">
                 <div class="col-25">
-                    <label for="fname">Naziv proizvoda:</label>
+                    <label for="proizvod">Naziv proizvoda:</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="fname" name="firstname" placeholder="Proizvod..">
+                    <input type="text" id="proizvod" name="proizvod" placeholder="Proizvod..">
                 </div>
             </div>
             <div class="row">
                 <div class="col-25">
-                    <label for="lname">Kolicina</label>
+                    <label for="kolicina">Kolicina</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="lname" name="lastname" placeholder="Kolicina..">
+                    <input type="text" id="kolicina" name="kolicina" placeholder="Kolicina..">
                 </div>
             </div>
             <div class="row">
                 <div class="col-25">
-                    <label for="country">Mjera</label>
+                    <label for="mjera">Mjera</label>
                 </div>
                 <div class="col-75">
-                    <select id="country" name="country">
+                    <select id="mjera" name="mjera">
                         <option value="kilogram">KG</option>
                         <option value="komad">Komad</option>
                         <option value="litar">L</option>
@@ -106,17 +108,72 @@ if ($result->num_rows == 0) {
             </div>
             <div class="row">
                 <div class="col-25">
-                    <label for="lname">Dodao</label>
+                    <label for="dodao">Dodao</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="lname" name="lastname" placeholder="Korisnik..">
+                    <input type="text" id="dodao" name="dodao" placeholder="Korisnik..">
                 </div>
             </div>
 
             <div class="row">
-                <input type="submit" value="Potvrdi">
+                <input type="submit" value="Potvrdi" id="btnDodaj">
             </div>
             <button type="button" id="ugasiDodavanje">Zatvori</button>
+        </form>
+    </div>
+
+
+    <div class="formaNoviProizvod" id="izmeniModel" style="display: none;">
+        <form action="#" name="IzmeniProizvod" method="post" id="izmeniForm">
+        <div class="row">
+                <div class="col-25">
+                    <label for="ID">ID:</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" id="idIzmena" name="idIzmena" >
+                </div>
+            </div>    
+        <div class="row">
+                <div class="col-25">
+                    <label for="proizvodIzmena">Naziv proizvoda:</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" id="proizvodIzmena" name="proizvodIzmena" placeholder="Proizvod..">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="kolicinaIzmena">Kolicina</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" id="kolicinaIzmena" name="kolicinaIzmena" placeholder="Kolicina..">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="mjeraIzmena">Mjera</label>
+                </div>
+                <div class="col-75">
+                    <select id="mjeraIzmena" name="mjeraIzmena">
+                        <option value="kilogram">KG</option>
+                        <option value="komad">Komad</option>
+                        <option value="litar">L</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="dodaoIzmena">Dodao</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" id="dodaoIzmena" name="dodaoIzmena" placeholder="Korisnik..">
+                </div>
+            </div>
+
+            <div class="row">
+                <input type="submit" value="Potvrdi" id="btnIzmeni">
+            </div>
+            <button type="button" id="ugasiIzmene">Zatvori</button>
         </form>
     </div>
 
@@ -127,22 +184,7 @@ if ($result->num_rows == 0) {
 
 
     <script src="js/home.js"></script>
-<script>  const showFormButton = document.getElementById('dodajNarudzbu');
-  const myForm = document.getElementById('dodajModel');
 
-  showFormButton.addEventListener('click', function() {
-    myForm.style.display = 'block'; // Prikaži formu kada je gumb kliknut
-  });
-  
-  const showFormButton2 = document.getElementById('ugasiDodavanje');
-  const myForm2 = document.getElementById('dodajModel');
-  showFormButton2.addEventListener('click', function() {
-    myForm2.style.display = 'none'; // Prikaži formu kada je gumb kliknut
-  });
-  
-  
-  
-  </script>
 
 </body>
 
--- js/home.js
+++ js/home.js
@@ -7,3 +7,96 @@ function prikazi() {
     }
   }
   
+  const showFormButton = document.getElementById('dodajNarudzbu');
+  const myForm = document.getElementById('dodajModel');
+
+  showFormButton.addEventListener('click', function() {
+      myForm.style.display = 'block'; 
+  });
+
+  const showFormButton2 = document.getElementById('ugasiDodavanje');
+  const myForm2 = document.getElementById('dodajModel');
+  showFormButton2.addEventListener('click', function() {
+      myForm2.style.display = 'none'; 
+  });
+
+
+
+  $(document).ready(function() {
+$('#btnDodaj').on('click', function() {
+
+var proizvod = $('#proizvod').val();
+var kolicina = $('#kolicina').val();
+var mjera = $('#mjera').val();
+var dodao = $('#dodao').val();
+if(proizvod!="" && kolicina!="" && mjera!="" && dodao!=""){
+$.ajax({
+  url: "handler/add.php",
+  type: "POST",
+  data: {
+    proizvod: proizvod,
+    kolicina: kolicina,
+    mjera: mjera,
+    dodao: dodao				
+  },
+  cache: false,
+  success: function(dataResult){
+    var dataResult = JSON.parse(dataResult);
+    if(dataResult.statusCode==200){
+      console.log("Ubacen proizvod");
+      
+      
+      alert("Ubacen novi proizvod na spisak!");			
+    }
+    else if(dataResult.statusCode==201){
+      alert("Desila se greska!");
+    }
+    
+  }
+});
+}
+else{
+  alert('Popunite sve polja !');
+}
+});
+});
+
+const showFormButton3 = document.getElementById('btnizmeniNarudzbu');
+const myForm3 = document.getElementById('izmeniModel');
+
+showFormButton3.addEventListener('click', function() {
+  const radioGumbi = document.getElementsByName("checked-donut");
+
+  for (let i = 0; i < radioGumbi.length; i++) {
+    if (radioGumbi[i].checked) {
+   myForm3.style.display = 'block'; 
+  break;
+    }
+  }
+  if(myForm3.style.display !='block'){
+  alert("Nije cekirana stavka za izmenu");
+}
+});
+
+const showFormButton4 = document.getElementById('ugasiIzmene');
+const myForm4 = document.getElementById('izmeniModel');
+showFormButton4.addEventListener('click', function() {
+    myForm4.style.display = 'none'; 
+});
+const polje = document.getElementById("idIzmena");
+
+polje.addEventListener("keydown", function(event) {
+  event.preventDefault();
+});
+var idZaIzmenu;
+function provjeriOznaku() {
+  const radioGumbi = document.getElementsByName("checked-donut");
+
+  for (let i = 0; i < radioGumbi.length; i++) {
+    if (radioGumbi[i].checked) {
+      document.getElementById("idIzmena").value=radioGumbi[i].value;
+      console.log(radioGumbi[i].value);
+      idZaIzmenu=radioGumbi[i].value;
+    }
+  }
+}
